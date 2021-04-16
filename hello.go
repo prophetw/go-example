7ae9f040d5cd7562cb847bda0f476cce76542bae
root <root@DESKTOP-I2ET75H.localdomain>
2021-04-16 14:48:28 +0800

    feat: upload file arr  save file

--- hello.go
+++ hello.go
@@ -4,17 +4,36 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"path/filepath"
 
 	"github.com/gin-gonic/gin"
 )
 
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+			c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+			c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+			c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+			c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+			if c.Request.Method == "OPTIONS" {
+					c.AbortWithStatus(204)
+					return
+			}
+			c.Next()
+	}
+}
 
 func main() {
 	// sh()
 	fmt.Println("Hello, World!")
+	pagesize := os.Getpid()
+	// hostname := os.Hostname()
+	fmt.Println(pagesize)
+	// fmt.Println(hostname)
 	gin.SetMode(gin.ReleaseMode)
 	router := gin.Default()
+	router.Use(CORSMiddleware())
 	router.GET("/ping", func(c *gin.Context) {
 		log.Println("hello")
 		res := sh()
@@ -22,17 +41,32 @@ func main() {
 			"message": res,
 		})
 	})
+	router.GET("/", func(c *gin.Context) {
+		log.Println("hello")
+		c.JSON(200, gin.H{
+			"message": "hello",
+		})
+	})
 	router.MaxMultipartMemory = 8 << 20  // 8 MiB
 	router.POST("/upload", func(c *gin.Context) {
 		// Multipart form
+		path, _ := os.Getwd()
+		log.Println("upload")
 		form, _ := c.MultipartForm()
-		files := form.File["upload[]"]
-
+		files := form.File["file[]"]
 		for _, file := range files {
-			log.Println(file.Filename)
+			log.Println(file)
 			filename := filepath.Base(file.Filename)
 			// Upload the file to specific dst.
-			c.SaveUploadedFile(file, filename)
+			dstFolderPath :=path+"/uploads" 
+			if _, err := os.Stat(dstFolderPath); os.IsNotExist(err) {
+				os.Mkdir(dstFolderPath, 0755)
+			}
+			log.Println(dstFolderPath)
+			err := c.SaveUploadedFile(file, dstFolderPath + "/" + filename)
+			if err != nil {
+				log.Println(err)
+			}
 		}
 		c.String(http.StatusOK, fmt.Sprintf("%d files uploaded!", len(files)))
 	})
